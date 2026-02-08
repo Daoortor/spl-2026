74f10dc27ea53304b818101c43d6623425bd956d
giperbolism <104669889+giperbolism@users.noreply.github.com>
2026-02-08 20:20:14 +0100

    Refactor NF_unique theorem with induction cases

--- TAPLFormalization/SemanticsTheorems.lean
+++ TAPLFormalization/SemanticsTheorems.lean
@@ -11,8 +11,25 @@ inductive isValue : Term → Prop where
 theorem isNF_iff_isValue : ∀ (t : Term), isNF t ↔ isValue t := by
   sorry
 
-theorem NF_unique : (t ~>* u) → (t ~>* u') → isNF u → isNF u' → u = u' := by
-  sorry
+theorem NF_unique (tu : t ~>* u) (tu' : t ~>* u') (nfu : isNF u) (nfu' : isNF u') : u = u' := by
+  induction tu with
+  | refl =>
+    induction tu' with
+    | refl => rfl
+    | tail b hb ih =>
+      rename_i aa bb cc dd
+      cases nfu cc b
+  | tail b hb ih  =>
+    induction tu' with
+    | refl =>
+      rename_i aa bb cc dd
+      cases nfu' bb b
+    | tail b hb ih =>
+      rename_i aa bb cc dd ee ff gg hh tt
+      let hh := smallStepDeterministic hh b
+      rw[hh] at hb
+      let ih := ih hb nfu
+      exact ih
 
 theorem termination : ∀ (t : Term), ∃ t', isNF t' ∧ t ~>* t' := by
   sorry
