55b5f8afc2c5367a4c9c3ac36b405e8536d15929
Daoortor <david.dshvsk@gmail.com>
2026-02-08 14:33:16 +0100

    add missing files to root

--- TAPLFormalization.lean
+++ TAPLFormalization.lean
@@ -1,3 +1,6 @@
 -- This module serves as the root of the `TaplFormalization` library.
 -- Import modules here that should be built as part of the library.
 import TAPLFormalization.AST
+import TAPLFormalization.Ctx
+import TAPLFormalization.Semantics
+import TAPLFormalization.SemanticsTheorems
